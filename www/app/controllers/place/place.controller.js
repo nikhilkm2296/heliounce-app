c440a85b34e9b4558c3f37fbf20ee4888ba72d4d
Nikhil KM <nikhilkm.2296@gmail.com>
2016-03-13 12:52:06 +0530

    Latest changes to the place reviews page

--- www/app/controllers/place/place.controller.js
+++ www/app/controllers/place/place.controller.js
@@ -33,25 +33,34 @@ function controller($scope, $ionicModal, PlaceService) {
         scope: $scope,
         animation: 'slide-in-up'
       }).then(function(modal) {
-        $scope.modal = modal;
+        $scope.reviews_modal = modal;
       });
-      $scope.openModal = function() {
-        $scope.modal.show();
+      $scope.openReviewsModal = function() {
+        $scope.reviews_modal.show();
       };
-      $scope.closeModal = function() {
-        $scope.modal.hide();
+      $scope.closeReviewsModal = function() {
+        $scope.reviews_modal.hide();
       };
       //Cleanup the modal when we're done with it!
       $scope.$on('$destroy', function() {
-        $scope.modal.remove();
+        $scope.reviews_modal.remove();
       });
-      // Execute action on hide modal
-      $scope.$on('modal.hidden', function() {
-        // Execute action
+
+      $ionicModal.fromTemplateUrl('place-add-review-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.add_review_modal = modal;
       });
-      // Execute action on remove modal
-      $scope.$on('modal.removed', function() {
-        // Execute action
+      $scope.openAddReviewModal = function() {
+        $scope.add_review_modal.show();
+      };
+      $scope.closeAddReviewModal = function() {
+        $scope.add_review_modal.hide();
+      };
+      //Cleanup the modal when we're done with it!
+      $scope.$on('$destroy', function() {
+        $scope.add_review_modal.remove();
       });
 
     vm.place.selectedPlace = selectedPlace;
